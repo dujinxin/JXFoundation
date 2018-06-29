969f11db4c78f536bf0b543ae276aab43ddbd4ad
dujinxin <510033463@qq.com>
2018-06-29 18:32:27 +0800

    no message

---
+++ Example/JXFoundation/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  JXFoundation
+//
+//  Created by dujinxin on 06/29/2018.
+//  Copyright (c) 2018 dujinxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
